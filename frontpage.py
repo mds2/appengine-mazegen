0e8193197186a372f46d959366669063b0c4c1f1
mds2 <michael.schuresko+github@gmail.com>
2015-05-20 23:45:14 -0700

    Got rid of user permissions and login.  Free and anonymous now.

--- frontpage.py
+++ frontpage.py
@@ -1,37 +1,29 @@
 import webapp2
 
-from google.appengine.api import users
-
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        user = users.get_current_user()
-
         self.response.headers['Content-Type'] = 'text/html'
         self.response.write ('<html> <head>')
         self.response.write ('<link rel="icon" href="statics/favicon.ico">')
         self.response.write ('</head><body>')
-        if user:
-            self.response.write ('<h2>Hello, ' + user.nickname() + '</h2>')
-            self.response.write ('<p>From here, you can download ...')
-            self.response.write ('<ul>')
-            self.response.write ('<li><a href="large_maze.ps">a large maze</a>'
-                                 '</li>')
-            self.response.write ('<li><a href="huge_maze.ps">an even larger maze'
-                                 '</a>'
-                                 '</li>')
-        else:
-            self.response.write ('<h2>Hello</h2>')
-            self.response.write ('<p><small>You can <a href="' +
-                                 users.create_login_url(self.request.uri) +
-                                 '">login</a>* to access larger mazes' +
-                                 '</small></p>'
-                )
-            self.response.write ('<p>Or you can download ...<ul>')
+        self.response.write ('<h2>Online maze generation,</h2>')
+        self.response.write ('<p>From here, you can download ...')
+        self.response.write ('<ul>')
         self.response.write ('<li><a href="medium_maze.ps">a medium maze</a>'
                              '</li>')
         self.response.write ('<li><a href="small_maze.ps">a small maze</a>'
                              '</li>')
+        self.response.write ('<li><a href="large_maze.ps">a large maze</a>'
+                             '</li>')
+        self.response.write ('<li><a href="huge_maze.ps">an even larger maze'
+                             '</a>'
+                             '</li>')
         self.response.write ('</ul><br/>')
+        self.response.write ('</p><p>')
+        self.response.write ('Note that downloading one of these mazes a second'
+                             ' (or third, etc.) time will result in a different'
+                             ' maze on each download.')
+        self.response.write ('</p>')
         self.response.write ('<p>(inspired by ')
         self.response.write ('<i><a '
                              'href="'
@@ -42,18 +34,12 @@ class MainPage(webapp2.RequestHandler):
         self.response.write ('<p>Basic code for maze generation available at '
                              '<a href="https://github.com/mds2/mazegen" '
                              'target="_blank">https://github.com/mds2/mazegen'
-                             '</a>')
+                             '</a></p>')
         self.response.write ('<p>Code to make this appengine page available at '
                              '<a href="https://github.com/mds2/appengine-mazegen'
                              '" target="_blank">'
-                             'https://github.com/mds2/appengine-mazegen</a>')
-        if not user:
-            self.response.write ('<br/><br/><p>* <small>'
-                                 'I could, theoretically, record'
-                                 ' which users had logged in to access large '
-                                 'mazes.'
-                                 ' So far I have been too lazy to write the '
-                                 'code to do so</small></p>')
+                             'https://github.com/mds2/appengine-mazegen</a></p>')
+
         self.response.write ('<hr/>')
         self.response.write ('<i><a href="http://mikeschuresko.blogspot.com/"'
                              ' target="_blank">'
